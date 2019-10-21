f55c31b82396dc8b5e47b3302c351e739201d921
IGdeadpool <xiongyifan84@gmail.com>
2019-10-21 12:11:59 +0800

    testPoker modification

--- Landlords/src/helpers/distribute.java
+++ Landlords/src/helpers/distribute.java
@@ -12,23 +12,24 @@ public class distribute{
 	public void washPoker(List<Poker> basePoker) {
 		Collections.shuffle(basePoker);
 	}
-	private List<Poker> pokerOnHand1=new ArrayList<Poker>(17);
-	private List<Poker> pokerOnHand2=new ArrayList<Poker>(17);
-	private List<Poker> pokerOnHand3=new ArrayList<Poker>(17);
-	private List<Poker> PokerForLandload=new ArrayList<Poker>(3);
-	
-	private List<List<Poker>> PokerGroup =new ArrayList<List<Poker>>(4);
 	public List<List<Poker>> distributePoker(List<Poker> basePoker){
+		List<Poker> pokerOnHand1=new ArrayList<Poker>(17);
+		List<Poker> pokerOnHand2=new ArrayList<Poker>(17);
+		List<Poker> pokerOnHand3=new ArrayList<Poker>(17);
+		List<Poker> PokerForLandload=new ArrayList<Poker>(3);//Poker for landload
+		
+		List<List<Poker>> PokerGroup =new ArrayList<List<Poker>>(4);
+		
 		washPoker(basePoker);
 		
 		for(int i=0;i<51;i++) {
 			pokerOnHand1.add(basePoker.get(i));
 			pokerOnHand2.add(basePoker.get(i++));
 			pokerOnHand3.add(basePoker.get(i++));
-		}
+		}//17*3
 		for(int i=51;i<54;i++) {
 			PokerForLandload.add(basePoker.get(i));
-		}
+		}//3
 		PokerGroup.add(pokerOnHand1);
 		PokerGroup.add(pokerOnHand2);
 		PokerGroup.add(pokerOnHand3);
--- Landlords/src/testEntities/testPoker.java
+++ Landlords/src/testEntities/testPoker.java
@@ -5,23 +5,122 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import entities.Poker;
-import enums.PokerRank;
+import entities.Hand;
 
+import enums.Rank;
+import enums.HandType;
 class TestPoker {
 
-	@Test
+	@Test//单牌大
 	void Should_ReturnTure_When_RankIsHigher() {
-		Poker poker1=new Poker(PokerRank.RANK_K, null);
-		Poker poker2=new Poker(PokerRank.RANK_Q, null);
+		Poker poker1=new Poker(Rank.RANK_K,null);
+		Poker poker2=new Poker(Rank.RANK_Q, null);
 		boolean result=poker1.compareTo(poker2);
 		assertEquals(result,true);
 	}
 	
-	@Test
-	void Should_ReturnFalso_When_RankIsLower() {
-		Poker poker1=new Poker(PokerRank.RANK_3, null);
-		Poker poker2=new Poker(PokerRank.RANK_10, null);
+	@Test//单牌小
+	void Should_ReturnFalse_When_RankIsLower() {
+		Poker poker1=new Poker(Rank.RANK_3, null);
+		Poker poker2=new Poker(Rank.RANK_10, null);
 		boolean result=poker1.compareTo(poker2);
 		assertEquals(result,false);
 	}
+	
+	@Test//对子大
+	void compareTo_Pair_When_RankIsHigher_True_Comparison() {
+		Hand hand1=new Hand(HandType.PAIR,Rank.RANK_2,2);
+		Hand hand2=new Hand(HandType.PAIR,Rank.RANK_J,2);
+		boolean result =true;//need to be modified(comparison between Pair)
+		assertEquals(result,true);
+	}
+	
+	@Test//对子小
+	void compareTo_Pair_When_RankIsLower_False_Comparison() {
+		Hand hand1=new Hand(HandType.PAIR,Rank.RANK_5,2);
+		Hand hand2=new Hand(HandType.PAIR,Rank.RANK_8,2);
+		boolean result =false;//need to be modified(comparison between Pair)
+		assertEquals(result,false);	
+	}
+	
+	@Test//三带大
+	void compareTo_Trio_When_RankIsHigher_True_Comparison() {
+		Hand hand1=new Hand(HandType.TRIO,Rank.RANK_A,null,3);
+		Hand hand2=new Hand(HandType.TRIO,Rank.RANK_K,null,3);
+		boolean result =true;//need to be modified(comparison between Trio)
+		assertEquals(result,true);
+	}
+	
+	@Test//三带小
+	void compareTo_Trio_When_RankIsLower_False_Comparison() {
+		Hand hand1=new Hand(HandType.TRIO,Rank.RANK_4,null,3);
+		Hand hand2=new Hand(HandType.TRIO,Rank.RANK_7,null,3);
+		boolean result =false;//need to be modified(comparison between Trio)
+		assertEquals(result,false);
+	}
+	
+	@Test//四带大
+	void compareTo_Quad_When_RankIsHigher_True_Comparison() {
+		Hand hand1=new Hand(HandType.QUAD,Rank.RANK_K,null,4);
+		Hand hand2=new Hand(HandType.QUAD,Rank.RANK_10,null,4);
+		boolean result =true;//need to be modified(comparison between Quad)
+		assertEquals(result,true);
+	}
+	
+	@Test//四带小
+	void compareTo_Quad_When_RankIsLower_False_Comparison() {
+		Hand hand1=new Hand(HandType.QUAD,Rank.RANK_5,null,4);
+		Hand hand2=new Hand(HandType.QUAD,Rank.RANK_9,null,4);
+		boolean result =false;//need to be modified(comparison between Quad)
+		assertEquals(result,false);		
+	}
+	
+	@Test//炸弹大
+	void compareTo_Bomb_When_RankIsHigher_True_Comparison() {
+		Hand hand1=new Hand(HandType.BOMB,Rank.RANK_2,4);
+		Hand hand2=new Hand(HandType.QUAD,Rank.RANK_8,4);
+		boolean result =true;//need to be modified(comparison between Bomb)
+		assertEquals(result,true);		
+	}
+	
+	@Test//炸弹小
+	void compareTo_Bomb_When_RankIsLower_False_Comparison() {
+		Hand hand1=new Hand(HandType.BOMB,Rank.RANK_5,null,4);
+		Hand hand2=new Hand(HandType.BOMB,Rank.RANK_9,null,4);
+		boolean result =false;//need to be modified(comparison between Bomb)
+		assertEquals(result,false);		
+	}
+	
+	@Test//顺子大
+	void compareTo_StraightOfSolo_When_RankIsHigher_True_Comparison() {
+		Hand hand1=new Hand(HandType.SOLO,Rank.RANK_10,5);
+		Hand hand2=new Hand(HandType.SOLO,Rank.RANK_4,5);
+		boolean result =true;//need to be modified(comparison between Straight)
+		assertEquals(result,true);	
+	}
+
+	@Test//顺子小
+	void compareTo_StraightOfSolo_When_RankIsLower_False_Comparison() {
+		Hand hand1=new Hand(HandType.SOLO,Rank.RANK_3,7);
+		Hand hand2=new Hand(HandType.SOLO,Rank.RANK_8,7);
+		boolean result =false;//need to be modified(comparison between Straight)
+		assertEquals(result,false);	
+	}
+
+	@Test//连对大
+	void compareTo_StraightOfPair_When_RankIsHigher_True_Comparison() {
+		Hand hand1=new Hand(HandType.PAIR,Rank.RANK_10,3);
+		Hand hand2=new Hand(HandType.PAIR,Rank.RANK_4,3);
+		boolean result =true;//need to be modified(comparison between Straight)
+		assertEquals(result,true);	
+	}
+
+	@Test//连对小
+	void compareTo_StraightOfPair_When_RankIsLower_False_Comparison() {
+		Hand hand1=new Hand(HandType.PAIR,Rank.RANK_3,4);
+		Hand hand2=new Hand(HandType.PAIR,Rank.RANK_8,4);
+		boolean result =false;//need to be modified(comparison between Straight)
+		assertEquals(result,false);	
+	}
+
 }
