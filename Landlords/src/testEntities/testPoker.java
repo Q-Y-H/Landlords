f35658df604e195364c862dc114dfd40bfb9dc94
HuangHHHHpai <50438540+HuangHHHHpai@users.noreply.github.com>
2019-10-21 19:50:34 +0800

    Update testPoker.java

--- Landlords/src/testEntities/testPoker.java
+++ Landlords/src/testEntities/testPoker.java
@@ -5,22 +5,22 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import entities.Poker;
-import enums.PokerRank;
+import enums.Rank;
 
 class TestPoker {
 
 	@Test
 	void Should_ReturnTure_When_RankIsHigher() {
-		Poker poker1=new Poker(PokerRank.RANK_K, null);
-		Poker poker2=new Poker(PokerRank.RANK_Q, null);
+		Poker poker1=new Poker(Rank.RANK_K, null);
+		Poker poker2=new Poker(Rank.RANK_Q, null);
 		boolean result=poker1.compareTo(poker2);
 		assertEquals(result,true);
 	}
 	
 	@Test
 	void Should_ReturnFalso_When_RankIsLower() {
-		Poker poker1=new Poker(PokerRank.RANK_3, null);
-		Poker poker2=new Poker(PokerRank.RANK_10, null);
+		Poker poker1=new Poker(Rank.RANK_3, null);
+		Poker poker2=new Poker(Rank.RANK_10, null);
 		boolean result=poker1.compareTo(poker2);
 		assertEquals(result,false);
 	}
