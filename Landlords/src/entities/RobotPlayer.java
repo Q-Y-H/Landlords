cb2731e531017b6ddae41cebc9009fa7ed8c3895
cuihuahuang <huangpa3@oocl.com>
2019-11-06 17:49:47 +0800

    xiaban

--- Landlords/src/entities/RobotPlayer.java
+++ Landlords/src/entities/RobotPlayer.java
@@ -1,11 +1,21 @@
 package entities;
 
+import java.util.*;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 
 import enums.HandType;
 import enums.PlayerRole;
-
+import enums.Rank;
+import enums.Suit;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import entities.*;
+import enums.*;
+import junit.framework.TestCase;
 
 public class RobotPlayer extends Player {
 	
@@ -74,7 +84,7 @@ public class RobotPlayer extends Player {
 		case BOMB:{
 			for (int i=3;i<18;i++) {
 				if(numOfCards[i]==4 && i>lastHand.getOrdinal())
-					response=
+
 			}
 		}
 		case SOLO:{
@@ -113,104 +123,100 @@ public class RobotPlayer extends Player {
 		return response;
 	}
 	
-	public void sparseCards() {		
-		//TO-DO:
-		List<Card> copyData=new ArrayList<Card>(cards);//copy of handCards
-		List<Card> temp=new ArrayList<Card>();
-		List<List<Card>> tempCollection=new ArrayList<List<Card>>();	
-		
-		//1. check if has bomb yes:cut the bomb out;	
-		
-		combinationSelect(tempCollection,copyData,temp, 4);
-		for(List<Card> combine: tempCollection) {
-			if(Hand.cards2hand(combine).getType()!=HandType.BOMB) {
-				tempCollection.remove(combine);
-			}
-		}
-		for(List<Card> combine:tempCollection) {
-			handList.add(Hand.cards2hand(combine));
-			copyData.removeAll(combine);
-		}
-		temp.clear();
-		tempCollection.clear();//generation
-		
-		//2. check if has 2 yes: cut 2s out;
-		
-		List<Card>collectionOf2s=new ArrayList<Card>();
-		combinationSelect(tempCollection,copyData,temp, 1);
-		for(List<Card> combine: tempCollection) {
-			if(Hand.cards2hand(combine).getOrdinal()==2) {
-				collectionOf2s.addAll(combine);
-			}
-		}
-		handList.add(Hand.cards2hand(collectionOf2s));
-		copyData.removeAll(collectionOf2s);
-		
-		temp.clear();
-		tempCollection.clear();//generation
-		
-		//3. check if has plane;
-		int countOf3=cards.size()/3*3;
-		for(int i=countOf3;i>=6;i-=3) {
-			combinationSelect(tempCollection,copyData,temp,i);
-			for(List<Card> combine: tempCollection) {
-				Hand tempHand=Hand.cards2hand(combine);
-				if(tempHand.getType()==HandType.TRIO&&tempHand.getChainLength()==i/3) {
-					handList.add(tempHand);
-					copyData.removeAll(combine);
-				}
-			}
-		}
-		temp.clear();
-		tempCollection.clear();
-		
-		//4. check if has straight of solo(always pick the longest)
-		List<List<Card>> collectionOfSOS =new ArrayList<List<Card>>();
-		for(int i=cards.size();i>=5;i--) {
-			combinationSelect(tempCollection,copyData,temp,i);
-			for(List<Card> combine: tempCollection) {
-				Hand tempHand=Hand.cards2hand(combine);
-				if(tempHand.getType()==HandType.SOLO) {
-					//handle
-					
-					
-					
-					
-					collectionOfSOS.add(combine);
-					copyData.removeAll(combine);
-				}
-			}
-		}
-		
-		//5. handle the SOS
-			
-		//5.1 若顺子中出现单牌（连续长度不限）且该单牌段长x，与顺子头部长度距离d1，尾部距离d2，满足：d1+x>=5且d2+x>=5，拆为两个顺子
-			
-		//5.2 顺子长度大于5，头/尾存在连对，顺子长度-连对长度>=5,转化为三带加顺子
-		//5.3 顺子长度大于5，头/尾存在单牌，顺子长度-连对长度>=5,转化为对子加顺子
-		//5.4 顺子周围存在多个对子/单牌。计算该区域totalHandCount，选择最小情况}直到无变化
-		//
+	public void sparseCards() {
+		List<Card> cardListForSparsing=new ArrayList<>(cards);
+		List<Hand> optimizingHandList;
+		this.handList=null;
+		stepsToWin=sparseCards(optimizingHandList,cardListForSparsing,0);
 	}
 	
-	
-	//穷尽后比较，输出满足条件的第一个List.(单牌对子ok,三带一输出为null，怀疑是三带一比较转换出现问题）另，需要添加炸弹识别
-		//need to be modified. ^_^
-		private static void combinationSelect(List<List<Card>> workspace,List<Card> dataList, List<Card> resultList, int length) {
-			List<Card> copyData;
-			List<Card> copyResult;
-
-			if(resultList.size() == length) {
-				workspace.add(resultList);
+	public int sparseCards(List<Hand> optimizingHandList,List<Card> cardListForSparsing,int formerSteps) {
+		int steps=formerSteps;	
+		int countTrio=0;
+		int countPair=0;
+		int countSolo=0;
+		if(cardListForSparsing.isEmpty())
+			return 0;
+		// Sparse Cards with minimum finishing steps
+		int[] numOfCards = new int[20];
+		for(Card card: cardListForSparsing) numOfCards[card.getRank().ordinal()+3]++;		
+		
+		if(numOfCards[16]!=0&&numOfCards[17]!=0) {
+			//Check Rocket
+			List <Card> bombList=Arrays.asList(c[17],c[16]);
+			optimizingHandList.add(Hand.cards2hand(bombList));
+			for(Card card:bombList) cardListForSparsing.remove(card);
+			steps+=1;
+		}
+		
+		for(int rank=0; rank<numOfCards.length; rank++) {
+			// Check Bomb
+			if(numOfCards[rank] == 4) {
+				List <Card> bombList=null;
+				for(int j=0;j<4;j++) bombList.add(c[rank]);
+				optimizingHandList.add(Hand.cards2hand(bombList));
+				for(Card card:bombList)cardListForSparsing.remove(card);
+				steps+=1;
 			}
-
-			for(int i = 0; i < dataList.size(); i++) {
-				copyData = new ArrayList<Card>(dataList);
-				copyResult = new ArrayList<Card>(resultList);
-
-				copyResult.add(copyData.get(i));
-				for(int j = i; j >=  0; j--)
-					copyData.remove(j);
-				combinationSelect(workspace,copyData, copyResult, length);
+			else if(numOfCards[rank]==3) {
+				countTrio++;
+			}
+			else if(numOfCards[rank]==2) {
+				countPair++;
+			}
+			else if(numOfCards[rank]==1) {
+				countSolo++;
 			}
 		}
+		
+		// Check plane
+		if(countTrio>=2) {
+			for(int rank=0; rank<numOfCards.length-1; rank++) {
+				if(numOfCards[rank]==3&&numOfCards[rank+1]==3)
+				List<Card> planeList=null;
+				List<Card> newSparseList=null;
+				//addPlane;
+				int tempStemp=sparseCards(optimizingHandList,newSparseList,steps);
+				if() {
+					
+				}
+			}
+		}
+		// Check 连对
+		// Check three+one
+		// Check 顺子
+		
+		// Check solo and pair
+		int[] numOfCards1 = new int[20];
+		for(Card card: cardListForSparsing) numOfCards1[card.getRank().ordinal()+3]++;
+		for(int rank=0; rank<numOfCards1.length; rank++) {
+			if(numOfCards1[rank] != 0) {
+				steps++;
+				List<Card> boringList=null;
+				for(int i=0;i<numOfCards1[rank];i++) boringList.add(c[rank]);
+				optimizingHandList.add(Hand.cards2hand(boringList));
+				for(Card card:boringList)cardListForSparsing.remove(card);
+			}
+		}
+		return steps;
+	}
+	
+	public int sortAccordingToWeight() {
+		
+	
+	}
+	
+	public int calculateAccordingToWeight() {
+		
+	}
+	
+	private Card[] c = new Card[20];
+	
+
+	@BeforeEach
+ 	//initial 17 cards
+    public void setUp() {
+	 	for(int i=3;i<18;i++)
+	 		c[i] = new Card(Rank.getRankByValue(i),Suit.BLANK);
+    
 }
