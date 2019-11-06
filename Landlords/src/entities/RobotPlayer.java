bf70d20f749341384619500b0e7f39ffa8907afc
cuihuahuang <huangpa3@oocl.com>
2019-11-06 10:10:15 +0800

    update by fanfan

--- Landlords/src/entities/RobotPlayer.java
+++ Landlords/src/entities/RobotPlayer.java
@@ -1,9 +1,11 @@
 package entities;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import enums.HandType;
 import enums.PlayerRole;
+
 
 public class RobotPlayer extends Player {
 	
@@ -55,7 +57,7 @@ public class RobotPlayer extends Player {
 	
 	public List<Card> playCardsPassively(List<Card> formerCards) {
 		List<Card> response = null;
-		sparseCards(formerCards);
+		sparseCards();
 		Hand lastHand=Hand.cards2hand(formerCards);
 		HandType lastHandType=lastHand.getType();
 		int[] numOfCards = new int[20];
@@ -113,5 +115,49 @@ public class RobotPlayer extends Player {
 	
 	public void sparseCards() {		
 		//TO-DO:
+		//1. check if has bomb yes:cut the bomb out;
+		List<List<Card>> bombCollection=new ArrayList<List<Card>>();		
+		List<Card> Temp=new ArrayList<Card>();
+		combinationSelect(bombCollection,cards,Temp, 4);
+		for(List<Card> combine: bombCollection) {
+			if(Hand.cards2hand(combine).getType()!=HandType.BOMB) {
+				bombCollection.remove(combine);
+			}
+		}
+		for(List<Card> combine:bombCollection) {
+			handList.add(Hand.cards2hand(combine));
+		}
+		//2. check if has 2 yes: cut 2s out;
+		
+		//3. check if has plane;
+		//4. check if has straight of solo(always pick the longest)
+		//5. handle the SOS{
+		//5.1 若顺子中出现单牌（连续长度不限）且该单牌段长x，与顺子头部长度距离d1，尾部距离d2，满足：d1+x>=5且d2+x>=5，拆为两个顺子
+		//5.2 顺子长度大于5，头/尾存在连对，顺子长度-连对长度>=5,转化为三带加顺子
+		//5.3 顺子长度大于5，头/尾存在单牌，顺子长度-连对长度>=5,转化为对子加顺子
+		//5.4 顺子周围存在多个对子/单牌。计算该区域totalHandCount，选择最小情况}直到无变化
+		//
 	}
+	
+	
+	//穷尽后比较，输出满足条件的第一个List.(单牌对子ok,三带一输出为null，怀疑是三带一比较转换出现问题）另，需要添加炸弹识别
+		//need to be modified. ^_^
+		private static void combinationSelect(List<List<Card>> workspace,List<Card> dataList, List<Card> resultList, int length) {
+			List<Card> copyData;
+			List<Card> copyResult;
+
+			if(resultList.size() == length) {
+				workspace.add(resultList);
+			}
+
+			for(int i = 0; i < dataList.size(); i++) {
+				copyData = new ArrayList<Card>(dataList);
+				copyResult = new ArrayList<Card>(resultList);
+
+				copyResult.add(copyData.get(i));
+				for(int j = i; j >=  0; j--)
+					copyData.remove(j);
+				combinationSelect(workspace,copyData, copyResult, length);
+			}
+		}
 }
